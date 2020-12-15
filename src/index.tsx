b049584ae6eeb00939d9102183723367d3e71eb3
TONI <eragatony@gmail.com>
2020-12-15 10:45:39 +0100

    finsihed remove feature

--- src/index.tsx
+++ src/index.tsx
@@ -14,12 +14,32 @@ const App = (): JSX.Element => {
 
     const handleSubmit = (e: FormEv): void => {
         e.preventDefault();
+        addTodo(value)
         setValue('')
     }
 
-    const addTodo = (text: string) => {
+    const addTodo = (text: string): void => {
         const newTodos: ITodo[] = [...todos, { text, complete: false }]
         setTodos(newTodos)
+    }
+
+    const completeTodo = (index: number): void => {
+        const newTodods: ITodo[] = [ ...todos]
+        newTodods[index].complete = !newTodods[index].complete
+        setTodos(newTodods)
+    }
+
+    const completeAll = (): void => {
+        const newTodos: ITodo[] = [...todos]
+        newTodos.forEach(todo => {
+            todo.complete = true
+        })
+    }
+
+    const removeTodo = (index: number): void => {
+        const newTodos: ITodo[] = [...todos]
+        newTodos.splice(index, 1)
+        setTodos(newTodos);
     }
 
     return (
@@ -29,6 +49,15 @@ const App = (): JSX.Element => {
                 <input type='text' required value={value} onChange={e => setValue(e.target.value)} />
                 <button type='submit'> Add Todo </button>
             </form>
+            <section>
+                {todos.map((todo: ITodo, index: number) => (
+                    <Fragment key={index}>
+                        <div style={{textDecoration: todo.complete ? 'line-through' :  '' }}> {todo.text} </div>
+                        <button type='button' onClick={() => completeTodo(index)}> {todo.complete ? 'Incomplete' : 'Complete'} </button>
+                        <button type='button' onClick={() => removeTodo(index)}> x </button> 
+                    </Fragment>
+                ))}
+            </section>
         </Fragment>
     )
 }
